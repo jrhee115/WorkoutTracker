671d23f00e31c9254d9b3d01cbab476978ba516e
J Rhee <jayrhee0514@gmail.com>
2020-12-20 16:06:22 -0500

    added server, copied it from rutgers notes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -16,4 +16,22 @@ router.put("/api/workouts/:id", ({body, params}, res) => {
     }).catch(err => {
         res.status(400).json(err);
     });
-})
+});
+
+router.get("/api/workouts", (req,res) => {
+    workout.find({}).then(dbworkout => {
+        res.json(dbworkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req,res) => {
+    workout.find({}).sort({ date: -1}).limit(7).then(dbworkout => {
+        res.json(dbworkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+module.exports = router; 
